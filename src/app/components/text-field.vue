d01df0bcb0cb66e5932d9a53a330f48d2a039cbc
persilee <i@lishaoy.net>
2022-01-08 14:50:44 +0800

    🥕 新增表单组件

---
+++ src/app/components/text-field.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>TextField</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TextField',
+
+  /**
+   * 属性
+   */
+  props: {
+    modelValue: {
+      type: String,
+    },
+
+    type: {
+      type: String,
+      default: 'text',
+    },
+  },
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {},
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: {},
+});
+</script>
+
+<style scoped></style>
